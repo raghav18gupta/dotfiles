8f6b7c9757aa7bbb4a187e68978fba9164f651d7
raghav18gupta <18raghavgupta@gmail.com>
2020-02-02 02:06:04 +0530

    Adding unsplash script

--- unsplash/unsplash.sh
+++ unsplash/unsplash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget -O /tmp/wallpaper-temp.jpg https://source.unsplash.com/1600x900
+wget -O /bin/unsplash/temp-wallpaper.jpg https://source.unsplash.com/1600x900
 
 if [ $? -eq 0 ]; then
-    mv /tmp/wallpaper-temp.jpg /tmp/wallpaper.jpg
-    gsettings set org.gnome.desktop.background picture-uri file:///tmp/wallpaper.jpg
-    gsettings set org.gnome.desktop.screensaver picture-uri file:///tmp/wallpaper.jpg
+    cp /bin/unsplash/temp-wallpaper.jpg /bin/unsplash/wallpaper.jpg
+    gsettings set org.gnome.desktop.background picture-uri /bin/unsplash/wallpaper.jpg
+    gsettings set org.gnome.desktop.screensaver picture-uri /bin/unsplash/wallpaper.jpg
 fi
